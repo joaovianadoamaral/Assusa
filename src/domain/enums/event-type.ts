979953eab15fb01f59003ba1439412562ed423db
Assusa Dev <dev@assusa.com.br>
2026-01-10 18:39:55 -0300

    feat(domain): adiciona enum EventType para eventos do sistema

---
+++ src/domain/enums/event-type.ts
@@ -0,0 +1,9 @@
+/**
+ * Tipo de evento no sistema
+ */
+export enum EventType {
+  SECOND_COPY_REQUEST = 'SECOND_COPY_REQUEST',
+  CONTACT_REQUEST = 'CONTACT_REQUEST',
+  OPEN_SITE = 'OPEN_SITE',
+  DELETE_DATA = 'DELETE_DATA',
+}
